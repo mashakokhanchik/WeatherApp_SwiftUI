8a688795394447d42624af2b9d5d7197c8ab60d7
Masha Kokhanchik <masha.kokhanchik@gmail.com>
2022-10-02 11:17:12 +0300

    Create project

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Мария Коханчик on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
